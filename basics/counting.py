f93bf9746a3b349292454d5f29e234abe22f8201
bob <jmontecarvalho@gmail.com>
2018-04-14 13:20:58 -0300

    gender prediction

--- basics/counting.py
+++ basics/counting.py
@@ -5,4 +5,5 @@ pprint.pprint(nltk.corpus.gutenberg.fileids())
 
 md = nltk.corpus.gutenberg.words("melville-moby_dick.txt")
 md_sents = nltk.corpus.gutenberg.sents("melville-moby_dick.txt")
+
 print(md_sents[3])
